c61b172bf787b53093edd20d1fb97df5dcbda254
stanzy0 <stanzyjoseph@gmail.com>
2026-08-08 09:43:58 +0100

    Fix /visitors/new server component auth check

--- app/visitors/new/page.tsx
+++ app/visitors/new/page.tsx
@@ -1,5 +1,5 @@
 import VisitorRegistrationWizard from '@/components/wizard/VisitorRegistrationWizard'
-import { getCurrentUser } from '@/lib/auth-client'
+import { getCurrentUser } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 export default async function NewVisitorPage() {
