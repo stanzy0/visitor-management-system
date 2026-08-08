df3e89a85835e4fe71cf62ce77ee16a308c878d5
stanzy0 <stanzyjoseph@gmail.com>
2026-08-08 09:53:32 +0100

    Make /visitors/new dynamic to preserve auth session

--- app/visitors/new/page.tsx
+++ app/visitors/new/page.tsx
@@ -2,6 +2,9 @@ import VisitorRegistrationWizard from '@/components/wizard/VisitorRegistrationWi
 import { getCurrentUser } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function NewVisitorPage() {
   const user = await getCurrentUser()
   if (!user) {
